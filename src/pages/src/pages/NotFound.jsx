dcfcb9994fdf661764abde70114f7a716034dd98
pdrana82732-dot <pdrana82732@gmail.com>
2026-05-25 15:36:04 +0530

    Add files via upload

---
+++ src/pages/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+export default function NotFound() {
+    return (
+        <div
+            style={{
+                paddingTop: "140px",
+                paddingBottom: "100px",
+                minHeight: "100vh",
+                textAlign: "center",
+            }}
+        >
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    )
+}
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import ScrollToTop from "../components/ScrollToTop"; // ← ADD THIS
+import ScrollToTop from "../components/ScrollToTop";
+
 import MainLayout from "../layouts/MainLayout";
+
 import Home from "../pages/Home";
 import Inventory from "../pages/Inventory";
 import Gallery from "../pages/Gallery";
 import Contact from "../pages/Contact";
 import ArtistBookingPage from "../pages/ArtistBooking";
+import NotFound from "../pages/NotFound";
 
 export default function AppRoutes() {
   return (
     <>
-      <ScrollToTop />  {/* ← ADD THIS */}
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<MainLayout />}>
 
@@ -18,9 +22,8 @@ export default function AppRoutes() {
           <Route path="inventory" element={<Inventory />} />
           <Route path="gallery" element={<Gallery />} />
           <Route path="contact" element={<Contact />} />
-
-          {/* Artist Booking Page */}
           <Route path="artist-booking" element={<ArtistBookingPage />} />
+          <Route path="*" element={<NotFound />} />
 
         </Route>
       </Routes>
